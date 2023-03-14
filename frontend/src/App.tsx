73f4557b758fac5c0a92bae1703f26e618c6b89d
Sanya <zaverisanya@gmail.com>
2023-03-15 03:05:12 +0530

    added components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,20 +6,27 @@ interface StyleProps {
 function App() {
   const cardStyle: StyleProps = {
     position:'relative',
-    left: '0%',
-    right: '0%',
+    left: '-4%',
+    right: '3%',
     top: '30.48%',
     bottom: '0%',
-    margin:'70.26px'
+    margin:'40px'
   }
 
   return (
     <div className='main' style={{ display: 'flex', justifyContent:'center'}}>
-      <AssetCard style={cardStyle} image={"/bitcoinsv.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/solana.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/ethereum.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/binance.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/shiba-inu.png"}></AssetCard>
+     <img src="/trending-asset.png" alt="" style={{position:'relative', width:'40px',height:'40px', left:'7.5%', top:'20%', border:'1.5px solid #DC1FFF'}}/>
+      <div className='my-text'>Trending Assets</div>
+      <AssetCard style={cardStyle} image={"/bitcoinsv.png"} name={"Bitcoin (BTC)"} 
+      price={"$31,812.80"} percentageClass={"profit"} percentage={"+10%"} image1={"/solana.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/solana.png"} name={"Solana (SOL)"}
+       price={"$32.83"} percentageClass={"loss"} percentage={"-12.32%"}  image1={"/bitcoinsv.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/ethereum.png" } name={"Ethereum (ETH)"} 
+      price={"$1,466.45"} percentageClass={"loss"} percentage={"-11.93%"}  image1={"/solana.png"} image2={"/bitcoinsv.png"} image3={"/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/binance.png"} name={"Binance USD (BUSD)"} 
+      price={"$1.00"} percentageClass={"profit"} percentage={"+0.26%"}  image1={"/solana.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/shiba-inu.png"} name={"Shiba Inu (SHIB)"} 
+      price={"$0.00000001948"} percentageClass={"loss"} percentage={"-8.1%"}  image1={"/solana.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
    </div >
   )
 }
--- frontend/src/components/AssetCard.tsx
+++ frontend/src/components/AssetCard.tsx
@@ -3,14 +3,39 @@ import './assetcard.css'
 import Circle from './Circle';
 
 interface CardProps{
+    percentageClass:string;
+    percentage:string;
+    name:string;
+    price:string;
     image?: string;
+    image1?: string;
+    image2?: string;
+    image3?: string;
     style?:React.CSSProperties;
 }
-const AssetCard = ({style,image}:CardProps) => {
+const AssetCard = ({style,image,name,price,percentageClass,percentage,image1,image2,image3}:CardProps) => {
 
     return (
             <div className="card" style={style}>
-                <Circle image={image}></Circle>            
+                <Circle image={image}></Circle>
+                <div className="card-name">{name}</div>     
+                <div className='rectangle'>
+                    <div className='price-text'>{price}</div>
+                    <div className={percentageClass}>{percentage}</div>
+                </div>     
+                <div className="text" >Price</div>      
+                <div className='rectangle' style={{top:'55%'}}>
+                    <div className='tvl-text'>$60,000</div>
+                </div>    
+                <div className="text" style={{top:'67%', fontSize:'12px'}}>TVL</div>  
+                <div className='sm-rectangle' style={{display:'flex', justifyContent:'center'}}>
+                    <div className='sm-circle' style={{margin:'5px', backgroundImage:`url(${image1})`}}></div>
+                    <div className='sm-circle' style={{margin:'5px', backgroundImage:`url(${image2})`}}></div>
+                    <div className='sm-circle' style={{margin:'5px', backgroundImage:`url(${image3})`}}></div>
+                </div>
+
+                
+                <div className="text" style={{top:'89%'}}>Popular pairs</div> 
             </div>
     )
 
