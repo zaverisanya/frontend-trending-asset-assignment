7e83219e730495bf81ceb6cd0b062a0f05f9110d
Sanya <zaverisanya@gmail.com>
2023-03-15 10:26:17 +0530

    changed images location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,16 +17,16 @@ function App() {
     <div className='main' style={{ display: 'flex', justifyContent:'center'}}>
      <img src="/trending-asset.png" alt="" style={{position:'relative', width:'40px',height:'40px', left:'7.5%', top:'20%', border:'1.5px solid #DC1FFF'}}/>
       <div className='my-text'>Trending Assets</div>
-      <AssetCard style={cardStyle} image={"frontend/public/bitcoinsv.png"} name={"Bitcoin (BTC)"} 
-      price={"$31,812.80"} percentageClass={"profit"} percentage={"+10%"} image1={"/solana.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/solana.png"} name={"Solana (SOL)"}
-       price={"$32.83"} percentageClass={"loss"} percentage={"-12.32%"}  image1={"/bitcoinsv.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/ethereum.png" } name={"Ethereum (ETH)"} 
-      price={"$1,466.45"} percentageClass={"loss"} percentage={"-11.93%"}  image1={"/solana.png"} image2={"/bitcoinsv.png"} image3={"/binance.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/binance.png"} name={"Binance USD (BUSD)"} 
-      price={"$1.00"} percentageClass={"profit"} percentage={"+0.26%"}  image1={"/solana.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
-      <AssetCard style={cardStyle} image={"/shiba-inu.png"} name={"Shiba Inu (SHIB)"} 
-      price={"$0.00000001948"} percentageClass={"loss"} percentage={"-8.1%"}  image1={"/solana.png"} image2={"/ethereum.png"} image3={"/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/assets/bitcoinsv.png"} name={"Bitcoin (BTC)"} 
+      price={"$31,812.80"} percentageClass={"profit"} percentage={"+10%"} image1={"/assets/solana.png"} image2={"/assets/ethereum.png"} image3={"/assets/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/assets/solana.png"} name={"Solana (SOL)"}
+       price={"$32.83"} percentageClass={"loss"} percentage={"-12.32%"}  image1={"/assets/bitcoinsv.png"} image2={"/assets/ethereum.png"} image3={"/assets/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/assets/ethereum.png" } name={"Ethereum (ETH)"} 
+      price={"$1,466.45"} percentageClass={"loss"} percentage={"-11.93%"}  image1={"/assets/solana.png"} image2={"/assets/bitcoinsv.png"} image3={"/assets/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/assets/binance.png"} name={"Binance USD (BUSD)"} 
+      price={"$1.00"} percentageClass={"profit"} percentage={"+0.26%"}  image1={"/assets/solana.png"} image2={"/assets/ethereum.png"} image3={"/assets/binance.png"}></AssetCard>
+      <AssetCard style={cardStyle} image={"/assets/shiba-inu.png"} name={"Shiba Inu (SHIB)"} 
+      price={"$0.00000001948"} percentageClass={"loss"} percentage={"-8.1%"}  image1={"/assets/solana.png"} image2={"/assets/ethereum.png"} image3={"/assets/binance.png"}></AssetCard>
    </div >
   )
 }
