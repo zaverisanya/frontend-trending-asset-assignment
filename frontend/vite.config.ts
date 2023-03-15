5204efd0ba5d267f2e9380c77dec0b05c8234ac7
Sanya <zaverisanya@gmail.com>
2023-03-15 14:13:50 +0530

    deployment changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: 'images',
+  },
   base:'/frontend-trending-asset-assignment/',
   plugins: [react()],
 })
